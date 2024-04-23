138aa71336d8ac9c9fd3a6570823f9c7f4ae860c
jgambard@alumni.nd.edu <jgambard@alumni.nd.edu>
2024-04-23 00:55:12 +0000

    staging flles

--- models/staging/stg_music__album.sql
+++
@@ -1,4 +0,0 @@
-select 
-artistid as artist_id,
-name as artist_name
-from {{ source("music",'artist')}}
---
+++ models/staging/stg_music__employees.sql
@@ -0,0 +1,16 @@
+select
+    employeeid as employee_id,
+    firstname as employee_first_name,
+    lastname as employee_last_name,
+    title as employee_title,
+    reportsto as supervisor_employee_id,
+    birthdate as employee_birth_date,
+    address as employee_address,
+    city as employee_city,
+    state as employee_state,
+    country as employee_country,
+    postalcode as employee_zipcode,
+    phone as employee_phone_number,
+    fax as employee_fax_number,
+    email as employee_email_address
+from {{ source("music", 'employee') }}
